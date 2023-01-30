63825d81db010659e1523b4336dce554ac1f3e20
Leonardo Zappi Ramos <leo.zappi@hotmail.com>
2023-01-30 11:21:18 -0300

    Init Front

--- Mentalista/back/main.py
+++ Mentalista/back/main.py
@@ -1,12 +1,4 @@
-from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-  
-@app.get("/admin")
-async def root_admin():
-  return {"message": "This is admin root"}
+if __name__ == '__main__':
+  uvicorn.run("app.api:app", host="127.0.0.1", port=8000, reload=True)
